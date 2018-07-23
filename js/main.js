4ab34160525239d2972f5688fbb466e9349f07a5
Philemon Berhane <philberhane@yahoo.com>
2018-07-23 13:47:27 -0700

    testing email

--- js/main.js
+++ js/main.js
@@ -42,65 +42,6 @@ card.addEventListener('change', function(event) {
 
 // Handle form submission.
 var form = document.getElementById('payment-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  // Creates a name property based on user input and tokenizes it
-  var card_name = document.getElementById('name').value
-
-  stripe.createToken(card, { name: card_name}).then(function(result) {
-    if (result.error) {
-      // Inform the user if there was an error.
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
-    } else {
-      
-        // Send the token to your server.
-
-        
-        //Creates an object to send to the server
-        const input = {
-        token : result.token.id,
-        email : document.getElementById('email').value,
-        }
-            
-        
-        
-        fetch("/updateCard", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(input)
-        })
-            .then(response => {
-                return response.json();
-            })
-        
-            .then(output => {
-            // Clears the form and renders a server response
-            document.getElementById('payment-form').reset()
-            card.clear()
-            document.getElementById('server-response').innerHTML = output.message
-            
-            
-            // Styles the response, success=blue, error=red
-            if (output.message.indexOf('Success') === 0) {
-                document.getElementById('server-response').style.color = '#238bb9'
-            } else {
-                document.getElementById('server-response').style.color = '#fa755a'
-            }
-            
-            
-            })
-
-            
-        
-        
-        
-        }   
-   
-    })
-      
-})
 
 
 
@@ -786,13 +727,9 @@ var chargeAmountWithDecimal = parseFloat(document.getElementById('paymentLabel')
         }   
    
     })
-    
+    window.location.href = "http://lilpeeps.shiftmediamanagement.com/thankyou.html";
 }
 
-
-function redirect() {
-    window.location.href = "http://127.0.0.1:50982/thankyou.html";
-}
 
 
 
